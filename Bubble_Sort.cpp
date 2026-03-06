a12c80282170ec682276de18ba3002af800b5541
Thaddea4 <adeyasyie@gmail.com>
2026-03-06 15:51:12 +0700

    Menambahkan prosedur input

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -6,5 +6,31 @@ int n;
 
 void input()
 {
-    cout
+
+    while (true)
+    {
+    cout << "Masukkan panjang array : ";
+    cin >> n;
+
+    if (n <= 20)
+    {
+        break;
+    }
+    else
+    {
+        cout << "Error : Maksimal input array adalah 20\n";
+    }
 }
+
+cout << endl;
+cout << "=====================" << endl;
+cout << "Masukkan elemen Array" << endl;
+cout << "=====================" << endl;
+
+for (int i = 0; i < n; i++) // i++ / i = i + 1
+{
+    cout << "Masukkan Nilai ke -" << i + 1 << " : ";
+    cin >> a[i];
+}
+}
+
