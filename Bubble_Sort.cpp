da4f873d1f5354c74958a95e588fbf947a041db1
Thaddea4 <adeyasyie@gmail.com>
2026-03-06 15:52:51 +0700

    Menambahkan prosedur display

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -34,3 +34,16 @@ for (int i = 0; i < n; i++) // i++ / i = i + 1
 }
 }
 
+void display()
+{
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "Elemen Array telah terurut" << endl;
+    cout << "==========================" << endl:
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke -" << i << " : " << a[i] << endl;
+    }
+}
+
